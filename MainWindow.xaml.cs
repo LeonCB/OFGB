4279d0053301e2190646ca7b655ce3b4ead17d64
LeonCB <leonbuitendam@gmail.com>
2026-05-13 19:20:40 +0200

    Test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Windows;
 using System.Windows.Interop;
 using System.Runtime.InteropServices;
 using System.Windows.Controls;
-using System.Security.Principal;
 
 namespace OFGB
 {
@@ -19,10 +18,14 @@ namespace OFGB
 
         const string cur_ver = "Software\\Microsoft\\Windows\\CurrentVersion\\";
 
+        // Prevents registry writes while checkboxes are being set during initialization
+        private bool _initializing = true;
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeKeys();
+            _initializing = false;
 
             DwmSetWindowAttribute(new WindowInteropHelper(Application.Current.MainWindow).EnsureHandle(), 33, [2], sizeof(int));
         }
@@ -30,92 +33,63 @@ namespace OFGB
         private void InitializeKeys()
         {
             // Sync provider notifications in File Explorer
-            bool key1 = CreateKey(cur_ver + "Explorer\\Advanced", "ShowSyncProviderNotifications");
-            cb1.IsChecked = key1;
+            cb1.IsChecked = ReadKey(cur_ver + "Explorer\\Advanced", "ShowSyncProviderNotifications");
 
             // Get fun facts, tips, tricks, and more on your lock screen
-            bool key2 = CreateKey(cur_ver + "ContentDeliveryManager", "RotatingLockScreenOverlayEnabled");
-            bool key3 = CreateKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-338387Enabled");
-            cb2.IsChecked = key2 && key3;
+            cb2.IsChecked = ReadKey(cur_ver + "ContentDeliveryManager", "RotatingLockScreenOverlayEnabled")
+                         && ReadKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-338387Enabled");
 
             // Show suggested content in Settings app
-            bool key4 = CreateKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-338393Enabled");
-            bool key5 = CreateKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-353694Enabled");
-            bool key6 = CreateKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-353696Enabled");
-            cb3.IsChecked = key4 && key5 && key6;
+            cb3.IsChecked = ReadKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-338393Enabled")
+                         && ReadKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-353694Enabled")
+                         && ReadKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-353696Enabled");
 
             // Get tips and suggestions when using Windows
-            bool key7 = CreateKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-338389Enabled");
-            cb4.IsChecked = key7;
+            cb4.IsChecked = ReadKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-338389Enabled");
 
             // Suggest ways to get the most out of Windows and finish setting up this device
-            bool key8 = CreateKey(cur_ver + "UserProfileEngagement", "ScoobeSystemSettingEnabled");
-            cb5.IsChecked = key8;
+            cb5.IsChecked = ReadKey(cur_ver + "UserProfileEngagement", "ScoobeSystemSettingEnabled");
 
-            // Show me the Windows welcome experience after updates and occasionally when I sign in to highlight what's new and suggested
-            bool key9 = CreateKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-310093Enabled");
-            cb6.IsChecked = key9;
+            // Show me the Windows welcome experience after updates
+            cb6.IsChecked = ReadKey(cur_ver + "ContentDeliveryManager", "SubscribedContent-310093Enabled");
 
             // Let apps show me personalized ads by using my advertising ID
-            bool key10 = CreateKey(cur_ver + "AdvertisingInfo", "Enabled");
-            cb7.IsChecked = key10;
+            cb7.IsChecked = ReadKey(cur_ver + "AdvertisingInfo", "Enabled");
 
             // Tailored experiences
-            bool key11 = CreateKey(cur_ver + "Privacy", "TailoredExperiencesWithDiagnosticDataEnabled");
-            cb8.IsChecked = key11;
+            cb8.IsChecked = ReadKey(cur_ver + "Privacy", "TailoredExperiencesWithDiagnosticDataEnabled");
 
             // "Show recommendations for tips, shortcuts, new apps, and more" on Start
-            bool key12 = CreateKey(cur_ver + "Explorer\\Advanced", "Start_IrisRecommendations");
-            cb9.IsChecked = key12;
+            cb9.IsChecked = ReadKey(cur_ver + "Explorer\\Advanced", "Start_IrisRecommendations");
 
-            // "Turn off notifications from <app>? We noticed you haven't opened these in a while."
-            bool key13 = CreateKey(cur_ver + "Notifications\\Settings\\Windows.ActionCenter.SmartOptOut", "Enabled");
-            cb10.IsChecked = key13;
+            // "Turn off notifications from <app>?"
+            cb10.IsChecked = ReadKey(cur_ver + "Notifications\\Settings\\Windows.ActionCenter.SmartOptOut", "Enabled");
 
-            // These Need To Be Run As Administrator
-            if (IsRunningAsAdministrator())
-            {
-                // Show Bing Results in Windows Search (Inverted, 1 == Disabled)
-                bool key14 = CreateKey("Software\\Policies\\Microsoft\\Windows\\Explorer", "DisableSearchBoxSuggestions");
-                bool key15 = CreateKey(cur_ver + "Search", "BingSearchEnabled");
-                cb11.IsChecked = !key14 && key15;
+            // Show Bing Results in Windows Search
+            // DisableSearchBoxSuggestions=1 means Bing is OFF, so checkbox (Bing ON) = inverted
+            bool bingDisabled = ReadKey("Software\\Policies\\Microsoft\\Windows\\Explorer", "DisableSearchBoxSuggestions");
+            bool bingEnabled  = ReadKey(cur_ver + "Search", "BingSearchEnabled");
+            cb11.IsChecked = !bingDisabled && bingEnabled;
 
-                // Disable Edge desktop search widget bar
-                bool key16 = CreateKey("Software\\Policies\\Microsoft\\Edge", "WebWidgetAllowed");
-                cb12.IsChecked = key16;
-            }
-            else
-            {
-                cb11.IsEnabled = false;
-                cb12.IsEnabled = false;
-            }
+            // Edge desktop search widget bar
+            cb12.IsChecked = ReadKey("Software\\Policies\\Microsoft\\Edge", "WebWidgetAllowed");
         }
 
-        private static bool CreateKey(string loc, string key)
+        /// <summary>
+        /// Reads a DWORD registry value under HKCU. Returns false if the key or value does not exist.
+        /// Does NOT create or write anything.
+        /// </summary>
+        private static bool ReadKey(string subKey, string valueName)
         {
-            RegistryKey? keyRef;
-            bool value;
+            using RegistryKey? key = Registry.CurrentUser.OpenSubKey(subKey, writable: false);
+            if (key is null)
+                return false;
 
-            if (Registry.CurrentUser.OpenSubKey(loc, true) is not null)
-            {
-                keyRef = Registry.CurrentUser.OpenSubKey(loc, true);
-            }
-            else
-            {
-                keyRef = Registry.CurrentUser.CreateSubKey(loc);
-                keyRef.SetValue(key, 0);
-            }
+            object? val = key.GetValue(valueName);
+            if (val is null)
+                return false;
 
-            if (keyRef is null)
-            {
-                MessageBox.Show("Failed to create a registry subkey during initialization!", "OFGB: Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                throw new InvalidOperationException("OFGB: Failed to create subkey during initialization!");
-            }
-
-            value = Convert.ToBoolean(keyRef.GetValue(key));
-            keyRef.Close();
-
-            return value;
+            return Convert.ToBoolean(val);
         }
 
         private static void ToggleOptions(string checkboxName, bool enable)
@@ -123,60 +97,58 @@ namespace OFGB
             switch (checkboxName)
             {
                 case "cb1":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Explorer\\Advanced\\", "ShowSyncProviderNotifications", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Explorer\\Advanced", "ShowSyncProviderNotifications", Convert.ToInt32(enable));
                     break;
                 case "cb2":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "RotatingLockScreenOverlayEnabled", Convert.ToInt32(!enable));
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-338387Enabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "RotatingLockScreenOverlayEnabled", Convert.ToInt32(enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-338387Enabled", Convert.ToInt32(enable));
                     break;
                 case "cb3":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-338393Enabled", Convert.ToInt32(!enable));
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-353694Enabled", Convert.ToInt32(!enable));
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-353696Enabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-338393Enabled", Convert.ToInt32(enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-353694Enabled", Convert.ToInt32(enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-353696Enabled", Convert.ToInt32(enable));
                     break;
                 case "cb4":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-338389Enabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-338389Enabled", Convert.ToInt32(enable));
                     break;
                 case "cb5":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "UserProfileEngagement", "ScoobeSystemSettingEnabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "UserProfileEngagement", "ScoobeSystemSettingEnabled", Convert.ToInt32(enable));
                     break;
                 case "cb6":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-310093Enabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "ContentDeliveryManager", "SubscribedContent-310093Enabled", Convert.ToInt32(enable));
                     break;
                 case "cb7":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "AdvertisingInfo", "Enabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "AdvertisingInfo", "Enabled", Convert.ToInt32(enable));
                     break;
                 case "cb8":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Privacy", "TailoredExperiencesWithDiagnosticDataEnabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Privacy", "TailoredExperiencesWithDiagnosticDataEnabled", Convert.ToInt32(enable));
                     break;
                 case "cb9":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Explorer\\Advanced", "Start_IrisRecommendations", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Explorer\\Advanced", "Start_IrisRecommendations", Convert.ToInt32(enable));
                     break;
                 case "cb10":
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Notifications\\Settings\\Windows.ActionCenter.SmartOptOut", "Enabled", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Notifications\\Settings\\Windows.ActionCenter.SmartOptOut", "Enabled", Convert.ToInt32(enable));
                     break;
                 case "cb11":
-                    Registry.SetValue("HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\Windows\\Explorer", "DisableSearchBoxSuggestions", Convert.ToInt32(enable)); // <- Inverted
-                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Search", "BingSearchEnabled", Convert.ToInt32(!enable));
+                    // Inverted: checkbox checked = Bing ON = DisableSearchBoxSuggestions 0, BingSearchEnabled 1
+                    Registry.SetValue("HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\Windows\\Explorer", "DisableSearchBoxSuggestions", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\" + cur_ver + "Search", "BingSearchEnabled", Convert.ToInt32(enable));
                     break;
                 case "cb12":
-                    Registry.SetValue("HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\Edge", "WebWidgetAllowed", Convert.ToInt32(!enable));
+                    Registry.SetValue("HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\Edge", "WebWidgetAllowed", Convert.ToInt32(enable));
                     break;
             }
         }
 
-        public static bool IsRunningAsAdministrator()
-        {
-            return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
-        }
-
         private void Checked(object sender, RoutedEventArgs e)
         {
+            if (_initializing) return;
             ToggleOptions(((CheckBox)sender).Name, true);
         }
 
         private void Unchecked(object sender, RoutedEventArgs e)
         {
+            if (_initializing) return;
             ToggleOptions(((CheckBox)sender).Name, false);
         }
 
